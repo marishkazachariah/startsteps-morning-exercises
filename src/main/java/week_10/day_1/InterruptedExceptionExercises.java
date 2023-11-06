5b95808c24731b76c311b370940513307ed2b996
marishkazachariah <marishka.zachariah@gmail.com>
2023-11-06 12:54:08 +0100

    complete exercise 2 of interrupted exceptions

--- src/main/java/week_10/day_1/InterruptedExceptionExercises.java
+++ src/main/java/week_10/day_1/InterruptedExceptionExercises.java
@@ -13,5 +13,27 @@ public class InterruptedExceptionExercises {
         });
 
         sleeperThread.start();
+
+        // Exercise 2
+        Thread longThread = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(i);
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Long thread interrupted.");
+                    return;
+                }
+            }
+        });
+
+        Thread interrupterThread = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                longThread.interrupt();
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted while sleeping: " + e.getMessage());
+            }
+        });
+        longThread.start();
+        interrupterThread.start();
     }
 }
